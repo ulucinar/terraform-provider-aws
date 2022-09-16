5c17307421ae77b1309dbf1e2d2ca049aeb80500
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2022-09-16 14:04:04 +0300

    Add native provider stderr logging in the terraform-provider-aws.sh script
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- terraform-provider-aws.sh
+++ terraform-provider-aws.sh
@@ -2,6 +2,6 @@
 
 # HO.
 
-LOG_DIR=/Users/alper/data/workspaces/xp/fix-ezgis
-#(($(dirname "${TERRAFORM_NATIVE_PROVIDER_PATH}")/terraform-provider-aws | tee -a ${LOG_DIR}/native-provider-stdout.log) 3>&1 1>&2 2>&3 | tee -a ${LOG_DIR}/native-provider-stderr.log) 3>&1 1>&2 2>&3
-"$(dirname "${TERRAFORM_NATIVE_PROVIDER_PATH}")/terraform-provider-aws" | tee -a ${LOG_DIR}/native-provider-stdout.log
+LOG_DIR=/Users/alper/data/workspaces/xp/fix-ezgis/logged-tests-8
+(($(dirname "${TERRAFORM_NATIVE_PROVIDER_PATH}")/terraform-provider-aws | tee -a ${LOG_DIR}/native-provider-stdout.log) 3>&1 1>&2 2>&3 | tee -a ${LOG_DIR}/native-provider-stderr.log) 3>&1 1>&2 2>&3
+#"$(dirname "${TERRAFORM_NATIVE_PROVIDER_PATH}")/terraform-provider-aws" | tee -a ${LOG_DIR}/native-provider-stdout.log

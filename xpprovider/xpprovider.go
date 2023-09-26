747dd79464bc3822e781bf2103ceb4cf08a82b82
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2023-09-26 22:24:26 +0300

    Rename xpprovider.Provider to xpprovider.GetProviderSchema
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- xpprovider/xpprovider.go
+++ xpprovider/xpprovider.go
@@ -5,16 +5,14 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
 )
 
 type AWSConfig conns.Config
 type AWSClient conns.AWSClient
 
-func Provider() *schema.Provider {
-	p, _ := provider.New(context.TODO())
-	return p
+func GetProviderSchema(ctx context.Context) (*schema.Provider, error) {
+	return provider.New(ctx)
 }
 
 func (ac *AWSConfig) GetClient(ctx context.Context, client *AWSClient) (*conns.AWSClient, diag.Diagnostics) {
